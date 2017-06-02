6d90f30669f6fdc810119e16e7e19fc0f829fde3
leoortiz@unochapeco.edu.br <leoortiz@unochapeco.edu.br>
2017-06-01 22:32:29 -0300

    Controller fakezera

--- www/index.php
+++ www/index.php
@@ -1,6 +1,8 @@
 <?php
 	require_once __DIR__ . "/source/application/DefinePath.php";
 	require_once APP_PATH . "/Connection.php";
+	require_once APP_PATH . "/Default.php";
+	require_once APP_PATH . "/NoobController.php";
 ?>
 <!DOCTYPE html>
 <html>
@@ -10,13 +12,7 @@
 <body>
 	<?php require_once HTML_PATH . "/header.php"; ?>
 	<main>
-		<?php
-			$request = explode('/',$_SERVER['REQUEST_URI'])[2];
-			if($request)
-				require_once "source/views/" .$request. ".php";
-			else
-				require_once "source/views/index.php";
-		?>
+		<?php new NoobController(); ?>
 	</main>
 </body>
 </html>
